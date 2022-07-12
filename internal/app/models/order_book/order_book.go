0446b1d8e9c89798e2b433200bacb58279341246
hiteshpattanayak-tw <hpattanayak@investvoyager.com>
2022-07-12 14:02:00 +0530

    Add orderbook model

---
+++ internal/app/models/order_book/order_book.go
@@ -0,0 +1,27 @@
+package order_book
+
+import (
+	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/models/book_keeping/book_side"
+	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/models/book_keeping/comparator"
+	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/models/order"
+)
+
+type OrderBook interface {
+	Get() (demands []*order.Order, supplies []*order.Order)
+	Update(demands []*order.Order, supplies []*order.Order) error
+}
+
+type orderBook struct {
+	demands  book_side.BookSide
+	supplies book_side.BookSide
+}
+
+func ProvideOrderBook(demandComparator, supplyComparator comparator.Comparator) OrderBook {
+	demandBookSide := book_side.ProvideOrderBookSide()
+	demandBookSide.SetComparator(demandComparator)
+
+	supplyBookSide := book_side.ProvideOrderBookSide()
+	supplyBookSide.SetComparator(supplyComparator)
+
+	return &orderBook{supplies: supplyBookSide, demands: demandBookSide}
+}

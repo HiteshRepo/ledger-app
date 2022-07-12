1bdc1b79051a0d2075aa9e5b34eeb29a972b4a4a
hiteshpattanayak-tw <hpattanayak@investvoyager.com>
2022-07-12 14:18:26 +0530

    Add ledger repo

---
+++ internal/app/repository/ledger.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/event_sourcing"
+	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/models/product"
+)
+
+type LedgerRepository struct {
+	inMemoryLedger map[string][]event_sourcing.Event
+}
+
+func NewWarehouseRepository() *LedgerRepository {
+	return &LedgerRepository{inMemoryLedger: make(map[string][]event_sourcing.Event)}
+}
+
+func (wr *LedgerRepository) Get(id string, name string) *product.Product {
+	newProduct := product.NewProduct(id, name)
+
+	if events, ok := wr.inMemoryLedger[id]; ok {
+		for _,e := range events {
+			_, _, _ = newProduct.AddEvent(e)
+		}
+	}
+
+	return newProduct
+}
+
+func (wr *LedgerRepository) Save(product *product.Product)  {
+	wr.inMemoryLedger[product.Id] = product.GetEvents()
+}

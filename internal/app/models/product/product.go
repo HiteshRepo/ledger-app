d456e89f769fc5141d87c52296170d18b5545aa3
hiteshpattanayak-tw <hpattanayak@investvoyager.com>
2022-07-12 14:16:16 +0530

    Add setter, getter methods to product model

--- internal/app/models/product/product.go
+++ internal/app/models/product/product.go
@@ -4,6 +4,7 @@ import (
 	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/event_sourcing"
 	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/models/book_keeping/comparator"
 	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/models/current_state"
+	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/models/order"
 	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/models/order_book"
 )
 
@@ -20,4 +21,24 @@ func NewProduct(id string, name string) *Product {
 		name:         name,
 		currentState: &current_state.CurrentState{OrderBook: order_book.ProvideOrderBook(comparator.ProvideDemandComparator(), comparator.ProvideSupplyComparator())},
 	}
+}
+
+
+func (p *Product) GetCurrentState() *current_state.CurrentState {
+	return p.currentState
+}
+
+func (p *Product) AddEvent(ev event_sourcing.Event) (error, *order.Order, *order.Order) {
+	err, matchDemand, matchSupply := ev.Apply(p.currentState)
+
+	if err != nil && err.Error() != "order did not match" {
+		return err, nil, nil
+	}
+
+	p.events = append(p.events, ev)
+	return nil, matchDemand, matchSupply
+}
+
+func (p *Product) GetEvents() []event_sourcing.Event {
+	return p.events
 }

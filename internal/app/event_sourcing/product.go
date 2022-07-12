f2dec3ff9fd270f531826ade7770f67d91944f4b
hiteshpattanayak-tw <hpattanayak@investvoyager.com>
2022-07-12 14:07:05 +0530

    Implement product supply event

--- internal/app/event_sourcing/product.go
+++ internal/app/event_sourcing/product.go
@@ -1,6 +1,12 @@
 package event_sourcing
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/models/current_state"
+	"github.com/hiteshpattanayak-tw/SupplyDemandLedger/internal/app/models/order"
+	"github.com/shopspring/decimal"
+	"log"
+)
 
 type productSupplyEvent struct {
 	id          uuid.UUID
@@ -18,4 +24,24 @@ func NewProductSupplyEvent(productName string, price, quantity float64) Event {
 		price:       price,
 		qty:         quantity,
 	}
+}
+
+func (pse productSupplyEvent) Apply(state *current_state.CurrentState) (error, *order.Order, *order.Order) {
+	newSupplyOrder := &order.Order{
+		Id:        uuid.New().String(),
+		Price:     decimal.NewFromFloat(pse.price),
+		Qty:       decimal.NewFromFloat(pse.qty),
+		OrderType: "supply",
+		Status:    "pending",
+	}
+
+	_ = state.OrderBook.Update(nil, []*order.Order{newSupplyOrder})
+
+	// match order
+
+	return nil, nil, nil
+}
+
+func (pse productSupplyEvent) Display() {
+	log.Printf("Supply order for product (%s) registered with quantity: %v, status: %s at %d\n", pse.productName, pse.qty, pse.status, pse.timestamp)
 }
